c1e81758e222c70e93001033148fd94d1ce7012a
unknown <azkaabdullah97gmail.comgit config --global user.name farah>
2024-04-07 08:34:06 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Slect one Operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// console.log(answer);
+// conditional statement if-else
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid input");
+}
